87555b729ea4c36cc4dc99c5c7f04898784d2706
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-03-13 00:11:20 +0100

    2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ul = document.querySelector("#ingredients");
+
+for (const ingredient of ingredients) {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  ul.insertAdjacentElement("afterbegin", li);
+}
+
+console.log(ul);
+ul.classList.add("item");
