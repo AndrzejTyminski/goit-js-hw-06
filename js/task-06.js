6ccb2341c59cdb1cba78a5778b3181237e35c483
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-03-16 18:59:08 +0100

    6

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,10 @@
 const inputText = document.querySelector("#validation-input");
-const valid = document.querySelector("#validation-input");
-const invalid = document.querySelector("#validation-input");
-
-//console.log(valid);
-
 const inputLength = inputText.getAttribute("data-length");
-console.log("punkt graniczny", inputLength);
 
 inputText.addEventListener("blur", (e) => {
   let temp = e.currentTarget.value.length;
-  console.log("wpisane znaki", temp);
-  //console.log("limit znakow", inputLength);
 
-  if (temp > inputLength) {
+  if (temp >= inputLength) {
     inputText.classList.remove("invalid");
     inputText.classList.add("valid");
   } else {
