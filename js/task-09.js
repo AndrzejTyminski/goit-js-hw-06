e6331482bcaba791bed82750b785c4c4502f7877
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-03-17 23:30:26 +0100

    9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+console.log(getRandomHexColor());
+
+const bodyEl = document.querySelector("body");
+
+const btnEl = document.querySelector("button");
+
+const change = () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  console.log(getRandomHexColor());
+};
+btnEl.addEventListener("click", change);
