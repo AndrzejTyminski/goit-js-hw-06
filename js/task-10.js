546a4ff467d655e9f6c79692e44ccd6eb18160a8
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-03-18 23:51:18 +0100

    10.6

--- js/task-10.js
+++ js/task-10.js
@@ -3,7 +3,6 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-
 const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-destroy]");
 const divBox = document.querySelector("#boxes");
@@ -12,11 +11,9 @@ divBox.style.height = "30px";
 
 const getInputValue = () => {
   let inputVal = document.querySelector("input").value;
-
   for (let i = 0; i < inputVal; i++) {
     const divEl = document.createElement("div");
     divBox.append(divEl);
-
     divEl.setAttribute(
       "style",
       "width: " +
@@ -26,17 +23,12 @@ const getInputValue = () => {
         (40 + i * 10) +
         "px;" +
         " background-color: " +
-        getRandomHexColor() +
-        "; margin: " +
-        1 +
-        "px"
+        getRandomHexColor()
     );
   }
 };
-
 const removeAct = () => {
   divBox.remove();
 };
-
 createBtn.addEventListener("click", getInputValue);
 destroyBtn.addEventListener("click", removeAct);
