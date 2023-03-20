c521eb5491d466e8f334f6264b50faef2d81479f
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-03-21 00:26:14 +0100

    skrocenie kodu

--- js/task-04.js
+++ js/task-04.js
@@ -1,20 +1,15 @@
 const dec = document.querySelector('button[data-action="decrement"]');
 const add = document.querySelector('button[data-action="increment"]');
-
 const counterValue = document.querySelector("#value");
 
 let i = 0;
 
 const clickListenerDec = () => {
-  i -= 1;
-  counterValue.textContent = i;
-  //console.log(i)
+  counterValue.textContent = --i;
 };
 
 const clickListenerAdd = () => {
-  i += 1;
-  counterValue.textContent = i;
-  //console.log(i)
+  counterValue.textContent = ++i;
 };
 
 dec.addEventListener("click", clickListenerDec);
