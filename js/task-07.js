4391f274ab646f95c5e9e2a001f465d4c8ead8dc
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-03-21 00:47:15 +0100

    czcionka nie zmienia gwaltownie swojego rozmiaru

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,8 @@
 const inputEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
 
+textEl.style.fontSize = "57px";
 inputEl.addEventListener("input", (e) => {
-  //let inputValue = e.currentTarget.value;
-
+  console.log(e.currentTarget.value);
   textEl.style.fontSize = e.currentTarget.value + "px";
 });
