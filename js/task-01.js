1eb640bd6b23a72c82e04426d835fca87f8e01b9
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-03-20 23:46:28 +0100

    1 usuniecie const categories linia 8

--- js/task-01.js
+++ js/task-01.js
@@ -5,10 +5,10 @@ console.log("Number od categories:", noc.length);
 
 // 2.
 
-const categories = document.querySelectorAll("h2");
+//const categories = document.querySelectorAll("h2");
 
 for (let i = 0; i <= noc.length; i++) {
-  const listit = categories[i].textContent;
+  const listit = noc[i].textContent;
   console.log("Category:", listit);
 
   const temp = document.querySelectorAll("h2 + ul");
