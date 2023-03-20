bce878b01f4e28715a5538db9714b68948db05ba
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-03-21 00:37:49 +0100

    po skasowaniu textu pojawia sie oryginalny napis, ale chyba prymitywna metoda to naprawilem

--- js/task-05.js
+++ js/task-05.js
@@ -3,4 +3,8 @@ const output = document.querySelector("#name-output");
 
 inp.addEventListener("input", (e) => {
   output.textContent = e.currentTarget.value;
+  console.log(output.textContent);
+  if (output.textContent == "") {
+    output.textContent = "Anonymous";
+  }
 });
