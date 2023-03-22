69a4938f8fc199bb10c3bab37e40c7f3e4c39540
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-03-22 13:57:32 +0100

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,21 @@
 // 1.
 
-const noc = document.querySelectorAll("h2");
-console.log("Number od categories:", noc.length);
+const noc = document.querySelectorAll(".item");
+console.log(`Number od categories:, ${noc.length}`);
 
 // 2.
 
 //const categories = document.querySelectorAll("h2");
 
-for (let i = 0; i <= noc.length; i++) {
-  const listit = noc[i].textContent;
-  console.log("Category:", listit);
+// for (let i = 0; i <= noc.length; i++) {
+//   const listit = noc[i].textContent;
+//   console.log("Category:", listit);
 
-  const temp = document.querySelectorAll("h2 + ul");
-  console.log("Elements:", temp[i].children.length);
+//   const temp = document.querySelectorAll("h2 + ul");
+//   console.log("Elements:", temp[i].children.length);
+// }
+
+for (const element of noc) {
+  console.log(`Category: ${element.children[0].innerText}`);
+  console.log(`Elements: ${element.children[1].children.length}`);
 }
