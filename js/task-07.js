02a0f381af5e7236776d19364c70d8a08c341946
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-03-22 11:16:14 +0100

    inputEl.value

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,10 @@
 const inputEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
 
-textEl.style.fontSize = "57px";
+
+const size = inputEl.value
+textEl.style.fontSize = size + "px";
+
 inputEl.addEventListener("input", (e) => {
   console.log(e.currentTarget.value);
   textEl.style.fontSize = e.currentTarget.value + "px";
